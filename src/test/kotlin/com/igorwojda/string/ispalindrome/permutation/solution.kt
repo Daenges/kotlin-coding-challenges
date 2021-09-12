29a1730eecb18703ccc5b3b0fbb5e2db5e427988
daenges <daenges2@gmail.com>
2021-09-12 20:33:42 +0200

    Add recursive permutation check

--- src/test/kotlin/com/igorwojda/string/ispalindrome/permutation/solution.kt
+++ src/test/kotlin/com/igorwojda/string/ispalindrome/permutation/solution.kt
@@ -10,3 +10,20 @@ private object Solution1 {
         return filtered.size <= 1
     }
 }
+
+// recursive solution
+private object Solution2 {
+    private fun isPermutationPalindrome(s: String) : Boolean {
+        return if(s.length > 1) {
+            val firstChar = s[0]
+            val restString = s.substring(1)
+            if (restString.contains(firstChar)) {
+                isPermutationPalindrome(restString.replaceFirst(firstChar.toString(), "", true))
+            } else {
+                false
+            }
+        } else {
+            true
+        }
+    }
+}
