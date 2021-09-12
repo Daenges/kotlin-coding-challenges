7979e7b2018e6d3ac28334caf6952761b2b7ace1
daenges <daenges2@gmail.com>
2021-09-12 20:41:46 +0200

    Add function to reverse int recursively

--- src/test/kotlin/com/igorwojda/integer/reverse/solution.kt
+++ src/test/kotlin/com/igorwojda/integer/reverse/solution.kt
@@ -4,8 +4,29 @@ import kotlin.math.sign
 
 private object Solution1 {
     private fun reverseInt(i: Int): Int {
-        var reverse = i.toString().removePrefix("-").reversed()
+        val reverse = i.toString().removePrefix("-").reversed()
         return reverse.toInt() * i.sign
+    }
+}
+// recursive approach
+private object Solution2 {
+    private fun reverseInt(num: Int) : Int {
+        val numAsStr = num.toString()
+        val firstCharOfNum : Char
+        val returnedInt : Int
+
+        if(numAsStr.length > 1) {
+            firstCharOfNum = numAsStr[0]
+            returnedInt = reverseInt(numAsStr.substring(1).toInt())
+        } else {
+            return num
+        }
+
+        return if(firstCharOfNum != '-') {
+            (returnedInt.toString() + firstCharOfNum.toString()).toInt()
+        } else {
+            (firstCharOfNum.toString() + returnedInt.toString()).toInt()
+        }
     }
 }
 
